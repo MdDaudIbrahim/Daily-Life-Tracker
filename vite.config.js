5b525c0c27f08e944ae142e28ff9e085f19b6c5c
Daud Ibrahim <daudibrahim029@gmail.com>
2025-02-21 03:51:46 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
-   base: "/Daily-Life-Tracker",
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/Daily-Life-Tracker/' : '/',
 });
